82a2a04b411681f2aad1c46a2499f3d0d0c21155
rline <crossrun000@gmail.com>
2021-10-21 22:23:10 +0900

    手順１４　移動したが矢印表示がうまくされない

--- Assets/Script/EnemyGenerator.cs
+++ Assets/Script/EnemyGenerator.cs
@@ -92,6 +92,30 @@ public class EnemyGenerator : MonoBehaviour
 
         //TODO 実装したい処理を日本語のコメントとして残しておくようにする
 
+        //移動する地点を取得(<=いままでEnemyControllerスクリプト内で行っていた処理をこちらに移動します)
+        Vector3[] paths = pathData.pathTranArray.Select(x => x.position).ToArray();
+
+        //敵キャラの初期設定を行い、移動を一時停止しておく
+        enemyController.SetUpEnemyController(paths);
+
+        //敵の移動経路のライン表示を生成の準備
+        StartCoroutine(PreparateCreatePathLine(paths, enemyController));
+
+    }
+
+     ///<summary>
+    ///ライン生成の準備
+    ///</summary>
+    ///<param name="paths"></param>
+    ///<returns></returns>
+    private IEnumerator PreparateCreatePathLine(Vector3[] paths, EnemyController enemyController)
+    {
+
+        //ラインの生成と削除。この処理が終了するまでは、この処理より下の処理は実行されない
+        yield return StartCoroutine(CreatePathLine(paths));
+
+        //敵の移動を再開
+        enemyController.ResumeMove();
     }
 
     /// <summary>
